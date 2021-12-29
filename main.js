46a7df5162991bd0aa7d8cd3c56bd8c00c34eec1
J13423 <91357604+J13423@users.noreply.github.com>
2021-12-29 14:35:55 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+canvas = new fabric.Canvas(myCanvas)
+// Create canvas variable
+ block_y=1;
+ block_x=1;
+
+block_image_width = 350;
+block_image_height = 430;
+
+var block_image_object= "";
+
+function new_image(gImage)
+{
+	// to upload images
+	fabric.image.fromURL(gImage, function(img) {
+		block_image_object.scaleToHeight(block_image_height);
+		block_image_width.scaleToWidth(block_image_width);
+		blockImage.set({top:block_y, left:block_x});
+	})
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '82') // add appropriate keycode
+	{
+		// upload red ranger
+		new_image(rr1.png);
+	}
+	if(keyPressed == '71')
+	{
+		block_x = 200;
+		// upload green ranger
+		new_image(gr.png);
+	}
+	
+	if(keyPressed == '89')
+	{
+		block_x =350;
+		// upload yellow ranger
+		new_image(yr.png);
+	}
+	if(keyPressed == '80')
+	{
+		block_x = 600;
+		new_image(pr.png);
+		// upload pink ranger
+	}
+	if(keyPressed == '66')
+	{
+		block_x = 700;
+		new_image(br.png);
+	// upload blue ranger
+	}
+	
+}
+
